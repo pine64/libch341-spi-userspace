324503f15b478182cf2728b8aa8e63d10dccbf16
Fossifous Acid <fossify+github@runbox.com>
2024-12-29 23:44:57 -0500

    Check return code of pinedio_init in test before continuing.

--- test.c
+++ test.c
@@ -8,7 +8,10 @@ void callback_test() {
 
 int main(int argc, char** argv) {
   struct pinedio_inst inst = pinedio_struct_default();
-  pinedio_init(&inst, NULL);
+  int ret;
+  if ((ret = pinedio_init(&inst, NULL)) < 0) {
+    return ret;
+  }
 #if 1
   for (int i = 0; i < 10; i++) {
 #if 1
